713ccc05ad46a2b6e630535dc84bf652a6a82092
Edaware Patricia <32902444+PatZino@users.noreply.github.com>
2018-08-03 01:56:23 +0100

    added exponential functions, numpyArray and made changes to passwordGenerator

--- exponential functions.py
+++ exponential functions.py
@@ -6,7 +6,7 @@ def exponentialFunc(x1,x2):
     result = (3 * math.pow((1-x1), 2))*math.exp(math.pow(x1, 2) + math.pow((x2+1), 2)) - \
              10 * ((x1/5)-math.pow(x1, 3) - math.pow(x2, 5)) * math.exp(math.pow(x1, 2) + math.pow(x2, 2)) - \
              ((1/3) * math.exp((math.pow((x1 + 1), 2) + math.pow(x2, 2))))
-    print(result)
+    print("the result of the exponential function = ", result)
 
 
 a = float(input("Enter a value for a: "))
