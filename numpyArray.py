71e533ae3e78dcff46f6afd2bbbcf352d32fdfdc
Edaware Patricia <32902444+PatZino@users.noreply.github.com>
2018-09-21 00:28:41 +0100

    modified numpyArray

--- numpyArray.py
+++ numpyArray.py
@@ -2,6 +2,34 @@ import numpy as np
 
 print("_"*50)
 
+mat0 = np.arange(4).reshape((2, 2))
+print("mat0: ", mat0)
+maxval0 = np.amax(mat0, axis=0)
+print("maxval0: ", maxval0)
+maxval01 = np.amax(mat0, axis=1)
+print("maxval01: ", maxval01)
+
+print("_"*50)
+
+mat1 = [[0, 1], [2, 3]]
+mat2 = [1, 2]
+matmul = np.matmul(mat1, mat2)
+print("matmul = ", matmul)
+maxval2 = np.amax(mat1, axis=0)
+maxval3 = np.amax(mat1, axis=0)
+print("maxval2: ", maxval2, "\n", "maxval3: ", maxval3)
+
+print("_"*50)
+
+mat3 = [[5, -1, 7], [-1, -1, 1], [7, 1, 5]]
+mat4 = [1, 1, 1]
+matmul = np.matmul(mat3, mat4)
+print("matmul = ", matmul)
+maxval = np.amax(matmul)
+print("maxval: ", maxval)
+
+print("_"*50)
+
 global p
 p = np.zeros((3, 3))
 
